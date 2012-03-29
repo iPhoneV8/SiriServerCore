7005b8c92c9f927a56e478bb733568222bf06960
Nicholas Bui <roxy_selindo@yahoo.co.id>
2012-03-29 12:55:11 +0700

    first commit

---
+++ config.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#This can be used as a module to set some config values for all classes
+#It is not thread safe! But is safe for per server run values
+forcelanguage=True
